f4b773afc02d0ccf655ca6e2549e1e5fda21b3b5
dnikolos <dnikolos@gmail.com>
2021-05-30 01:44:22 +0300

    Add npm packaging

---
+++ package.js
@@ -0,0 +1,18 @@
+package =
+{
+    "author": "Φωτόδεντρο (http://photodentro.edu.gr)",
+    "browser": "index.html",
+    "bugs": "http://photodentro.edu.gr/lor/feedback/form.jsp",
+    "description": "Μηχανισμός σκιάς",
+    "descriptionLong": "Δυναμική οπτική αναπαράσταση με στόχο την κατανόηση του μηχανισμού δημιουργίας σκιάς και του αμετάβλητου των σχετικών θέσεων, εμποδίων και σκιών. \nΤο μαθησιακό αντικείμενο δίνει τη δυνατότητα στους μαθητές να πειραματιστούν με τη φωτεινή πηγή, μεταβάλλοντας την απόστασή της από το εμπόδιο, και να δοκιμάσουν να δημιουργήσουν σκιές με διαφορετικά αντικείμενα ως εμπόδια.",
+    "homepage": "http://photodentro.edu.gr/lor/r/8521/10778",
+    "icon": "package.jpg",
+    "keywords": [
+        "σκιά",
+        "φωτεινή πηγή"
+    ],
+    "license": "CC-BY-SA-4.0",
+    "name": "8521-10778",
+    "repository": "http://photodentro.edu.gr/lor/retrieve/58532/shadow_v1.0_pidx0055148.zip",
+    "version": "1.0.0"
+}
